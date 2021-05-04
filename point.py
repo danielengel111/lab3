f6498d66179090d4363c9112aca47c210997d17e
maayan <maayanmagal@gmail.com>
2021-05-04 16:56:19 +0300

    l1

--- point.py
+++ point.py
@@ -9,7 +9,14 @@ class Point:
         if not self.coordinates:
             print('Point', self.name, 'not initiated. Please provide coordinates in init or call set_coordinates')
             return 0
-        return sum([(my-his)**2 for my, his in zip(self.coordinates, coordinates)])**0.5
+        dis=0
+        for my, his in zip(self.coordinates, coordinates):
+            if((my-his)<0):
+                dis+=his-my
+            else:
+                dis+=my-his
+        return dis
+        #return sum([(my-his) for my, his in zip(self.coordinates, coordinates)])
 
     def set_coordinates(self, coordinates):
         self.coordinates = [float(x) for x in coordinates]
